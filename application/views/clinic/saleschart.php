92d13852b1689e6a8d8e9879c2c22ae54cd22c71
geisher09 <geisherbernabe@gmail.com>
2018-02-25 06:14:56 +0800

    sales chart2

--- application/views/clinic/saleschart.php
+++ application/views/clinic/saleschart.php
@@ -27,30 +27,53 @@
 				        	var date = [];
 				        	var total_cost = [];
 
+				        	var visitdate = [];
+				        	var visit_cost = [];
+
 
 				        	for(var i in obj.sales2){
 				        		date.push("Date " + obj.sales2[i].date);
 				        		total_cost.push(obj.sales2[i].total_cost);
 				        	}
 
+				        	for(var i in obj.sales1){
+				        		visitdate.push("Date " + obj.sales1[i].visitdate);
+				        		visit_cost.push(obj.sales1[i].visit_cost);
+				        	}
+
 				        	var chartdata = {
 								labels: date,
 								datasets : [
 									{
-										label: 'Date Cost',
+										label: 'ITEMS',
 										backgroundColor: 'black',
 										borderColor: 'rgba(200, 200, 200, 0.75)',
 										hoverBackgroundColor: 'rgba(200, 200, 200, 1)',
 										hoverBorderColor: 'rgba(200, 200, 200, 1)',
+										fill : false,
+										lineTension : 0,
+										pointRadius : 5,
 										data: total_cost
+									},
+									{
+										label: 'VISIT',
+										backgroundColor: 'red',
+										borderColor: 'rgba(200, 200, 200, 0.75)',
+										hoverBackgroundColor: 'rgba(200, 600, 200, 1)',
+										hoverBorderColor: 'rgba(200, 200, 200, 1)',
+										fill : false,
+										lineTension : 0,
+										pointRadius : 5,
+										data: visit_cost
 									}
+
 								]
 							};
 
 							var ctx = $("#mycanvas");
 
 							var barGraph = new Chart(ctx, {
-								type: 'bar',
+								type: 'line',
 								data: chartdata
 							});   
 
