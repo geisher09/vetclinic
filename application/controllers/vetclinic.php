bb03fcbde57290858c820c51ca64a1f8bb9e95c4
dlvg22 <30533251+dlvg22@users.noreply.github.com>
2018-03-08 03:50:41 +0700

    remove click on radio buttons

--- application/controllers/vetclinic.php
+++ application/controllers/vetclinic.php
@@ -291,7 +291,7 @@ class vetclinic extends CI_Controller {
 								'qty'=>$data['item'],
 								'total_cost'=>$prd);
 				$this->itemhistory->create($input);
-			echo 'hio';
+			echo $notif;
 	}
 
 	public function savehistory(){
